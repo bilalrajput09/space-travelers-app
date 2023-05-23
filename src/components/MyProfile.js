eb34d1d2b02b5ee3ce25d3fa7003faa412b498bf
Adams-Ghub <bamfoadamsf@gmail.com>
2023-05-23 09:35:39 +0000

    Display joined missions on profile page

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -3,9 +3,26 @@ import styles from './styles/MyProfile.module.css';
 
 const MyProfile = () => {
   const rocketsArray = useSelector((state) => state.rockets.rocket);
+  const { missions } = useSelector((state) => state.mission);
+
   const myRockets = rocketsArray.filter((rocket) => rocket.reserved === true);
+  const myMissions = missions.filter((mission) => mission.reserved === true);
   return (
-    <section>
+    <section className={styles.profile_main_container}>
+      <main className={styles.my_missions_main_container}>
+        <header>My Missions</header>
+        <article>
+          {myMissions.length === 0 ? (
+            <span>No Mission joined!</span>
+          ) : (
+            <ul className={styles.list}>
+              {myMissions.map((mission) => (
+                <li key={mission.mission_id}>{mission.mission_name}</li>
+              ))}
+            </ul>
+          )}
+        </article>
+      </main>
       <main className={styles.my_rockets_main_container}>
         <header>My Rockets</header>
         <article>
