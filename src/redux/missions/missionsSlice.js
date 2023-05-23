13ad7db60e75b4eec516594080c7fa10ea49064d
Adams-Ghub <bamfoadamsf@gmail.com>
2023-05-23 08:01:48 +0000

    Update branch

--- src/redux/missions/missionsSlice.js
+++ src/redux/missions/missionsSlice.js
@@ -1,20 +1,35 @@
 import { createSlice } from '@reduxjs/toolkit';
 import getAllMission from './missionActions';
 
-const initialState = {
+const missionsState = {
   missions: [],
 };
 const missionsSlice = createSlice({
   name: 'mission',
-  initialState,
-  reducer: {
-
+  initialState: missionsState,
+  reducers: {
+    joinMission: (state, { payload }) => {
+      state.missions = state.missions.map((mission) => {
+        if (mission.mission_id !== payload) {
+          return mission;
+        }
+        return { ...mission, reserved: true };
+      });
+    },
+    leaveMission: (state, { payload }) => {
+      state.missions = state.missions.map((mission) => {
+        if (mission.mission_id !== payload) {
+          return mission;
+        }
+        return { ...mission, reserved: false };
+      });
+    },
   },
   extraReducers: {
     [getAllMission.fulfilled]: (state, { payload }) => {
-      state.missions = payload;
+      state.missions = [...payload];
     },
   },
 });
-export const missionAction = missionsSlice.actions;
+export const { joinMission, leaveMission } = missionsSlice.actions;
 export default missionsSlice.reducer;
